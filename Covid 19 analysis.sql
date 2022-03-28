aab580ecb3b0bc57397fb3b06ab1e71650ccb885
Anh-Dinh <94047158+Anh-Dinh@users.noreply.github.com>
2022-03-29 02:22:15 +0700

    Update Covid 19 analysis.sql

--- Covid 19 analysis.sql
+++ Covid 19 analysis.sql
@@ -1,4 +1,3 @@
-
 Select *
 From CovidDeaths
 Where continent is not null 
@@ -27,7 +26,7 @@ order by 1,2;
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From CovidDeaths
---Where location like '%states%'
+-- Where location like '%states%'
 order by 1,2;
 
 
@@ -35,7 +34,7 @@ order by 1,2;
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From CovidDeaths
---Where location like '%states%'
+-- Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc;
 
@@ -44,7 +43,7 @@ order by PercentPopulationInfected desc;
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From CovidDeaths
---Where location like '%states%'
+-- Where location like '%states%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc;
@@ -57,7 +56,7 @@ order by TotalDeathCount desc;
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From CovidDeaths
---Where location like '%states%'
+-- Where location like '%states%'
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc;
@@ -68,9 +67,9 @@ order by TotalDeathCount desc;
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From CovidDeaths
---Where location like '%states%'
+-- Where location like '%states%'
 where continent is not null 
---Group By date
+-- Group By date
 order by 1,2;
 
 
@@ -80,7 +79,7 @@ order by 1,2;
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
+-- , (RollingPeopleVaccinated/population)*100
 From CovidDeaths dea
 Join CovidVaccinations vac
 	On dea.location = vac.location
@@ -96,13 +95,13 @@ as
 (
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
+-- , (RollingPeopleVaccinated/population)*100
 From CovidDeaths dea
 Join CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
---order by 2,3
+-- order by 2,3
 )
 Select *, (RollingPeopleVaccinated/Population)*100
 From PopvsVac;
@@ -125,13 +124,13 @@ RollingPeopleVaccinated numeric
 Insert into #PercentPopulationVaccinated
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
+-- , (RollingPeopleVaccinated/population)*100
 From CovidDeaths dea
 Join CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
---where dea.continent is not null 
---order by 2,3;
+-- where dea.continent is not null 
+-- order by 2,3;
 
 Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
@@ -144,7 +143,7 @@ From #PercentPopulationVaccinated
 Create View PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
+-- , (RollingPeopleVaccinated/population)*100
 From CovidDeaths dea
 Join CovidVaccinations vac
 	On dea.location = vac.location
